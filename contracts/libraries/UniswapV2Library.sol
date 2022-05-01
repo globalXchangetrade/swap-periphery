a33dd84c483e0e3d810e43ec1e60eb8c68a1a431
Daksh Joshi <64137744+dakshjoshi@users.noreply.github.com>
2022-05-02 02:43:03 +0530

    initial commit

--- contracts/libraries/UniswapV2Library.sol
+++ contracts/libraries/UniswapV2Library.sol
@@ -21,7 +21,7 @@ library UniswapV2Library {
                 hex'ff',
                 factory,
                 keccak256(abi.encodePacked(token0, token1)),
-                hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
+                hex'0856b88077f6ddc8cc64f37ec8fe488d3b07d081444166b8d7c2fe6503f0819d' // init code hash
             ))));
     }
 
